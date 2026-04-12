6307c7686e09c3d7976b5979e1f2dca1a930cc79
Estate Nest Capital <hello@estatenest.capital>
2026-04-11 22:10:52 -0600

    Umber OS foundation: schema, proxy, core UI primitives

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,10 @@
+﻿import * as React from "react";
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input(
+  { className, ...props },
+  ref
+) {
+  return <input ref={ref} className={className} {...props} />;
+});
